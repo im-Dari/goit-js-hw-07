05156bf4c7cd861938343d1f6607e20379afaf8f
Dasha <licht@MacBook-Air-Dasha.local>
2025-12-06 22:52:22 +0100

    add file

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesList = document.getElementById('categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItems.length}`);
+categoriesItems.forEach(item => {
+const categoryTitle = item.querySelector('h2').textContent;
+const elementsCount = item.querySelectorAll('ul li').length;
+
+console.log(`Category: ${categoryTitle}`);
+console.log(`Elements: ${elementsCount}`);
+});
